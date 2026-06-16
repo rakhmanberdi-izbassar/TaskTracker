b4f2cab7859bffacbaafc962915ba5cd1409420e
rakhmanberdi-izbassar <rakhmanberdi.izbassar@narxoz.kz>
2026-06-16 14:21:34 +0500

    first commit

---
+++ internal/model/task.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	UserID      int       `json:"user_id"`
+	DueDate     time.Time `json:"due_date"`
+	Priority    int       `json:"priority"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
